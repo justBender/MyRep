938628de66f432dd864d34da658c2ef0bbe17983
justBender <bender1.9@gmx.net>
2019-03-28 01:10:01 +0100

    even more new stuff

--- scripts/main.js
+++ scripts/main.js
@@ -85,3 +85,48 @@ function itemKeypress(event) {
     updateItem.call(this);
   }
 }*/
+
+import $ from "jquery";
+
+function showError(e) {
+  console.warn("Error", e);
+}
+
+function updateUI(info) {
+  $("#app").text(JSON.stringify(info));
+}
+
+function getLocationURL([city, state]) {
+  return `https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22${city}%2C%20${state}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`;
+}
+
+function getUser(id) {
+  return new Promise((resolve, reject) => {
+    $.getJSON({
+      url: `https://api.github.com/users/${id}`,
+      success: resolve,
+      error: reject
+    });
+  });
+}
+
+function getWeather(user) {
+  return new Promise((resolve, reject) => {
+    $.getJSON({
+      url: getLocationURL(user.location.split(",")),
+      success(weather) {
+        resolve({ user, weather: weather.query.results });
+      },
+      error: reject
+    });
+  });
+}
+
+$("#btn").on("click", () => {
+  getUser("tylermcginnis")
+    .then(getWeather)
+    .then(data => {
+      updateUI(data);
+    })
+    .catch(showError);
+});
